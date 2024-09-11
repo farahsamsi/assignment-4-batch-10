fa815b240ef743f3332c1388b9816abd0fcb179f
farahsamsi <1827034farahsamsiprome@gmail.com>
2024-09-11 23:02:44 +0600

    problem 5

--- index.js
+++ index.js
@@ -86,28 +86,58 @@ function calculateFinalScore(obj) {
   return result;
 }
 
-console.log(
-  calculateFinalScore({
-    name: "Rajib",
-    testScore: 45,
-    schoolGrade: 25,
-    isFFamily: true,
-  })
-);
-console.log(
-  calculateFinalScore({
-    name: "Rajib",
-    testScore: 45,
-    schoolGrade: 25,
-    isFFamily: false,
-  })
-);
-console.log(calculateFinalScore("hello"));
-console.log(
-  calculateFinalScore({
-    name: "Rajib",
-    testScore: 15,
-    schoolGrade: 25,
-    isFFamily: true,
-  })
-);
+// console.log(
+//   calculateFinalScore({
+//     name: "Rajib",
+//     testScore: 45,
+//     schoolGrade: 25,
+//     isFFamily: true,
+//   })
+// );
+// console.log(
+//   calculateFinalScore({
+//     name: "Rajib",
+//     testScore: 45,
+//     schoolGrade: 25,
+//     isFFamily: false,
+//   })
+// );
+// console.log(calculateFinalScore("hello"));
+// console.log(
+//   calculateFinalScore({
+//     name: "Rajib",
+//     testScore: 15,
+//     schoolGrade: 25,
+//     isFFamily: true,
+//   })
+// );
+
+/** Problem 5 */
+function waitingTime(waitingTimes, serialNumber) {
+  if (
+    Array.isArray(waitingTimes) === false ||
+    typeof serialNumber !== "number" ||
+    serialNumber < waitingTimes.length
+  ) {
+    return "Invalid Input";
+  }
+
+  let sum = 0;
+  for (let time of waitingTimes) {
+    sum = sum + time;
+  }
+  let average = Math.round(sum / waitingTimes.length);
+  let remainingNumberCandidate = serialNumber - waitingTimes.length - 1;
+  let timeRequired = average * remainingNumberCandidate;
+
+  return timeRequired;
+}
+
+console.log(waitingTime([3, 5, 7, 11, 6], 10));
+console.log(waitingTime([13, 2], 6));
+console.log(waitingTime([13, 2, 6, 7, 10], 6));
+console.log(waitingTime([6], 4));
+
+console.log(waitingTime(7 , 10));
+console.log(waitingTime("[6,2]", 9));
+console.log(waitingTime([7, 8, 3, 4, 5], "9"));
