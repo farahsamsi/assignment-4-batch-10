803c4cbcc819491a6810daf5b76b885ba942791f
farahsamsi <1827034farahsamsiprome@gmail.com>
2024-09-11 22:35:45 +0600

    problem 4

--- index.js
+++ index.js
@@ -17,16 +17,15 @@ function calculateTax(income, expenses) {
 // console.log(calculateTax(6000, -1500));
 // console.log(calculateTax(6000, 8000));
 
-
 /** Problem 2 */
-function sendNotification(email){
-    let splittingEmail = email.split('@');
-    if(splittingEmail.length !== 2){
-        return 'Invalid Email';
-    }
-    let userName = splittingEmail[0];
-    let domainName = splittingEmail[1];
-    return userName + ' sent you an email from ' + domainName;   
+function sendNotification(email) {
+  let splittingEmail = email.split("@");
+  if (splittingEmail.length !== 2) {
+    return "Invalid Email";
+  }
+  let userName = splittingEmail[0];
+  let domainName = splittingEmail[1];
+  return userName + " sent you an email from " + domainName;
 }
 
 // console.log(sendNotification('zihad@gmail.com'));
@@ -36,28 +35,79 @@ function sendNotification(email){
 // console.log(sendNotification('sadia8icloud.com'));
 // console.log(sendNotification('sadia8ic@loud@.com'));
 
-
-
 /** Problem 3 */
 function checkDigitsInName(name) {
-    if(typeof name !== 'string'){
-        return 'Invalid Input';
+  if (typeof name !== "string") {
+    return "Invalid Input";
+  }
+  let testArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  let result = false;
+  for (let char of name) {
+    if (testArray.includes(char)) {
+      result = true;
     }
-    let testArray = ['1','2','3','4','5','6','7','8','9','0'];
-    let result = false;
-    for (let char of name){
-        if(testArray.includes(char)){
-            result = true;
-        }
-    }
-    return result;
+  }
+  return result;
 }
 
-console.log(checkDigitsInName('Raj123'));
-console.log(checkDigitsInName('n9ayeem'));
-console.log(checkDigitsInName('e1mu3'));
-console.log(checkDigitsInName('Suman'));
-console.log(checkDigitsInName('Name2024'));
-console.log(checkDigitsInName('!@#'));
-console.log(checkDigitsInName(["Raj"]));
-console.log(checkDigitsInName("R.aj"));
+// console.log(checkDigitsInName('Raj123'));
+// console.log(checkDigitsInName('n9ayeem'));
+// console.log(checkDigitsInName('e1mu3'));
+// console.log(checkDigitsInName('Suman'));
+// console.log(checkDigitsInName('Name2024'));
+// console.log(checkDigitsInName('!@#'));
+// console.log(checkDigitsInName(["Raj"]));
+// console.log(checkDigitsInName("R.aj"));
+
+/** Problem 4 */
+function calculateFinalScore(obj) {
+  let result = false;
+  if (
+    typeof obj !== "object" ||
+    typeof obj.name !== "string" ||
+    obj.testScore > 50 ||
+    typeof obj.testScore !== "number" ||
+    obj.schoolGrade > 30 ||
+    typeof obj.schoolGrade !== "number" ||
+    typeof obj.isFFamily !== "boolean"
+  ) {
+    return "Invalid Input";
+  }
+
+  let finalScore = 0;
+  if (obj.isFFamily === true) {
+    finalScore = obj.testScore + obj.schoolGrade + 20;
+  } else {
+    finalScore = obj.testScore + obj.schoolGrade;
+  }
+  if (finalScore >= 80) {
+    result = true;
+  }
+  return result;
+}
+
+console.log(
+  calculateFinalScore({
+    name: "Rajib",
+    testScore: 45,
+    schoolGrade: 25,
+    isFFamily: true,
+  })
+);
+console.log(
+  calculateFinalScore({
+    name: "Rajib",
+    testScore: 45,
+    schoolGrade: 25,
+    isFFamily: false,
+  })
+);
+console.log(calculateFinalScore("hello"));
+console.log(
+  calculateFinalScore({
+    name: "Rajib",
+    testScore: 15,
+    schoolGrade: 25,
+    isFFamily: true,
+  })
+);
